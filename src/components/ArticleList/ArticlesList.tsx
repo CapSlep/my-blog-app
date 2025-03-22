370f22dc96dc933bc1174f16e220090623ddd2fe
CapSlep <caps0307@gmail.com>
2025-03-22 21:03:59 +0100

    made sorting of articles

--- src/components/ArticleList/ArticlesList.tsx
+++ src/components/ArticleList/ArticlesList.tsx
@@ -1,14 +1,90 @@
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Button } from "react-bootstrap";
 import "./ArticlesList.scss";
 import { ArticleCard } from "../../components";
 import { ArticleData } from "../../types";
 
 interface articleListProps {
-  articles: ArticleData[] | null;
+  articles: ArticleData[] | undefined;
   isLoading: boolean;
 }
 
+enum SortMethods {
+  upvotes = "upvotes",
+  comments = "comments",
+  time = "time",
+  default = "default",
+}
+
 function ArticlesList({ articles, isLoading }: articleListProps) {
+  const [articlesList, setArticlesList] = useState<ArticleData[] | undefined>(
+    undefined
+  );
+  const [lastSortMethod, setLastSortMethod] = useState<SortMethods>(
+    SortMethods.default
+  );
+
+  useEffect(() => {
+    setArticlesList(articles);
+  }, [articles]);
+
+  function sortArticles(sortMethod: SortMethods) {
+    switch (sortMethod) {
+      case SortMethods.upvotes:
+        if (lastSortMethod === SortMethods.upvotes) {
+          setArticlesList(articlesList?.slice().reverse());
+          break;
+        }
+        setArticlesList(
+          articles?.slice().sort((a, b) => {
+            return b.upvotes - a.upvotes;
+          })
+        );
+        setLastSortMethod(SortMethods.upvotes);
+        break;
+
+      case SortMethods.comments:
+        if (lastSortMethod === SortMethods.comments) {
+          setArticlesList(articlesList?.slice().reverse());
+          break;
+        }
+        setArticlesList(
+          articles?.slice().sort((a, b) => {
+            return b.comments.length - a.comments.length;
+          })
+        );
+        setLastSortMethod(SortMethods.comments);
+        break;
+
+      case SortMethods.time:
+        if (lastSortMethod === SortMethods.time) {
+          setArticlesList(articlesList?.slice().reverse());
+          break;
+        }
+        setArticlesList(
+          articles?.slice().sort((a, b) => {
+            return (
+              new Date(b.creationDate).getTime() -
+              new Date(a.creationDate).getTime()
+            );
+          })
+        );
+        setLastSortMethod(SortMethods.time);
+        break;
+
+      case SortMethods.default:
+        setArticlesList(articles);
+        setLastSortMethod(SortMethods.default);
+        break;
+
+      default:
+        console.error("Invalid sort method");
+        setArticlesList(articles);
+        setLastSortMethod(SortMethods.default);
+        break;
+    }
+  }
+
   return (
     <Container className="article__list-container">
       {isLoading ? (
@@ -19,7 +95,30 @@ function ArticlesList({ articles, isLoading }: articleListProps) {
         </>
       ) : (
         <>
-          {articles?.map((a: ArticleData) => {
+          <div className="gap-2 d-flex justify-content-start">
+            <Button
+              onClick={() => {
+                sortArticles(SortMethods.upvotes);
+              }}
+            >
+              Sort by Upvotes
+            </Button>
+            <Button
+              onClick={() => {
+                sortArticles(SortMethods.comments);
+              }}
+            >
+              Sort by Comments
+            </Button>
+            <Button
+              onClick={() => {
+                sortArticles(SortMethods.time);
+              }}
+            >
+              Sort by Time
+            </Button>
+          </div>
+          {articlesList?.map((a: ArticleData) => {
             return (
               <ArticleCard
                 key={a.name}
